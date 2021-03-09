dfdb13068180c24d60c5024dc67fb656133a15cf
tarikb <bayzin@hotmail.com.tr>
2021-03-09 17:32:13 +0300

    structure

---
+++ src/main/java/com/client/CallRestService.java
@@ -0,0 +1,15 @@
+package com.client;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class CallRestService {
+
+    private static void CallRestService() {
+
+        RestTemplate restTemplate = new RestTemplate();
+
+
+    }
+}
