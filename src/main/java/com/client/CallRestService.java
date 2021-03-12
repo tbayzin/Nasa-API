b03ccc4bfc65f32996d646b9f8914d3626682c5e
tarikb <bayzin@hotmail.com.tr>
2021-03-12 17:08:16 +0300

    hash maple cekme yapıyor

--- src/main/java/com/client/CallRestService.java
+++ src/main/java/com/client/CallRestService.java
@@ -5,14 +5,23 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 @Component
 public class CallRestService implements CommandLineRunner {
 
     private static void CallRestService() {
 
         RestTemplate restTemplate = new RestTemplate();
-        Planet planet = restTemplate.getForObject ("https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0/1",Planet.class);
-        System.out.println("Gezegenler " + planet.getFirstUtc());
+        // Objenin adı string olduğu için ilki string ve içindekilerde obje olduğu için ikincisi Object
+        HashMap<String, Object> planet = restTemplate.getForObject ("https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0/",HashMap.class);
+
+        // Verilen objedeki numarayı alır
+        System.out.println("Gezegenler " + planet.get("807"));
+
+
+
     }
 
     @Override
--- src/main/java/com/model/Planet.java
+++ src/main/java/com/model/Planet.java
@@ -8,41 +8,41 @@ import javax.xml.bind.annotation.XmlRootElement;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Planet {
 
-    String firstUtc;
-    String lastUtc;
-    int monthOrdinal ;
-    String northernSeason;
+    String First_UTC;
+    String Last_UTC;
+    int Month_ordinal;
+    String Northern_season;
 
-    public String getFirstUtc() {
-        return firstUtc;
+    public String getFirst_UTC() {
+        return First_UTC;
     }
 
-    public void setFirstUtc(String firstUtc) {
-        this.firstUtc = firstUtc;
+    public void setFirst_UTC(String first_UTC) {
+        this.First_UTC = first_UTC;
     }
 
-    public String getLastUtc() {
-        return lastUtc;
+    public String getLast_UTC() {
+        return Last_UTC;
     }
 
-    public void setLastUtc(String lastUtc) {
-        this.lastUtc = lastUtc;
+    public void setLast_UTC(String last_UTC) {
+        this.Last_UTC = last_UTC;
     }
 
-    public int getMonthOrdinal() {
-        return monthOrdinal;
+    public int getMonth_ordinal() {
+        return Month_ordinal;
     }
 
-    public void setMonthOrdinal(int monthOrdinal) {
-        this.monthOrdinal = monthOrdinal;
+    public void setMonth_ordinal(int month_ordinal) {
+        this.Month_ordinal = month_ordinal;
     }
 
-    public String getNorthernSeason() {
-        return northernSeason;
+    public String getNorthern_season() {
+        return Northern_season;
     }
 
-    public void setNorthernSeason(String northernSeason) {
-        this.northernSeason = northernSeason;
+    public void setNorthern_season(String northern_season) {
+        this.Northern_season = northern_season;
     }
 //  Pre pre;
   //  Wd wd;
